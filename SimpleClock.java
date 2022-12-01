53c931411e64397ed75007d8975c5b9bee208dfd
zprongua <115650795+zprongua@users.noreply.github.com>
2022-12-01 09:59:02 -0500

    added button to switch between 12/24 hr format

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,11 +2,14 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.awt.event.ItemEvent;
 
 
-public class SimpleClock extends JFrame {
+public class SimpleClock extends JFrame implements ItemListener {
     
         Calendar calendar;
         SimpleDateFormat timeFormat;
@@ -16,6 +19,7 @@ public class SimpleClock extends JFrame {
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
+        JToggleButton militaryToggle;
         String time;
         String day;
         String date;
@@ -24,7 +28,7 @@ public class SimpleClock extends JFrame {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 220);
+            this.setSize(450, 220);
             this.setResizable(false);
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
@@ -40,17 +44,20 @@ public class SimpleClock extends JFrame {
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
-    
+
+            militaryToggle = new JToggleButton("24hr");
+            militaryToggle.addItemListener(this);
+
             this.add(timeLabel);
             this.add(dayLabel);
+            this.add(militaryToggle);
             this.add(dateLabel);
             this.setVisible(true);
     
             setTimer();
         }
-    
-        public void setTimer() {
+
+    public void setTimer() {
             while (true) {
                 time = timeFormat.format(Calendar.getInstance().getTime());
                 timeLabel.setText(time);
@@ -71,4 +78,16 @@ public class SimpleClock extends JFrame {
         public static void main(String[] args) {
             new SimpleClock();
         }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (militaryToggle.isSelected()) {
+            militaryToggle.setText("12hr");
+            timeFormat = new SimpleDateFormat("   kk:mm:ss   ");
+        }
+        else {
+            militaryToggle.setText("24hr");
+            timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        }
     }
+}
